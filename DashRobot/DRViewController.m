cbed41995f5a25cf5aff210b11d3b4148fb5cc4f
Antoine Balaresque <antoinebalaresque@berkeley.edu>
2013-08-31 12:34:16 -0700

    First commit

--- DashRobot/DRViewController.m
+++ DashRobot/DRViewController.m
@@ -10,9 +10,27 @@
 
 @interface DRViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *rightMotorValue;
+@property (strong, nonatomic) IBOutlet UITextField *leftMotorValue;
+
+
 @end
 
 @implementation DRViewController
+
+@synthesize rightMotorValue = _rightMotorValue;
+@synthesize leftMotorValue = _leftMotorValue;
+int rightMotor;
+int leftMotor;
+
+- (IBAction)rightSliderMoved:(UISlider *)sender {
+    rightMotor = sender.value;
+    self.rightMotorValue.text = [NSString stringWithFormat:@"%i", rightMotor];
+}
+- (IBAction)leftSliderMoved:(UISlider *)sender {
+    leftMotor = sender.value;
+    self.leftMotorValue.text = [NSString stringWithFormat:@"%i", leftMotor];
+}
 
 - (void)viewDidLoad
 {
@@ -26,4 +44,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)rightMotor:(UISlider *)sender {
+}
 @end
